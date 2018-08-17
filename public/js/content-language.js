b6bf90aa7a0aa01a3e8b157d577893e7eeeb9e87
Samu Juvonen <samu.juvonen@kirjastot.fi>
2018-08-17 12:20:10 +0300

    Expand translated fields when form has errors

--- public/js/content-language.js
+++ public/js/content-language.js
@@ -1,22 +1,25 @@
 (function($) {
   "use strict";
 
-  function toggle_active_language(event) {
-    var langcode = event.currentTarget.value;
-
-    console.log(langcode);
+  function toggle_active_language(form, langcode) {
+    form = $(form);
 
     if (langcode) {
-      $('[data-app-group="content-language"]').addClass('collapsed');
-      $('[data-content-language]').hide();
-      $('[data-content-language="' + langcode + '"]').show();
+      $('[data-app-group="content-language"]', form).addClass('collapsed');
+      $('[data-content-language]', form).hide();
+      $('[data-content-language="' + langcode + '"]', form).show();
     } else {
-      $('[data-content-language]').show();
-      $('[data-app-group="content-language"]').removeClass('collapsed');
+      $('[data-content-language]', form).show();
+      $('[data-app-group="content-language"]', form).removeClass('collapsed');
     }
   }
 
   $("[data-app=\"content-language\"]").each(function(i, input) {
-    $(input).on("change", toggle_active_language);
+    $(input).on("change", () => toggle_active_language(input.form, input.value));
+
+    // Expand translatable fields to allow native error popups to show up.
+    $(input.form).find(":input").on("invalid", (event) => {
+      toggle_active_language(input.form, null);
+    });
   });
 }(jQuery));
