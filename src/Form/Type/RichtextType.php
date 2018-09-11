d2072518d0cf619bea2faf4505a340e44452998a
Samu Juvonen <samu.juvonen@kirjastot.fi>
2018-09-11 17:14:47 +0300

    Convert CKEditor's pseudo-empty value to NULL

--- src/Form/Type/RichtextType.php
+++ src/Form/Type/RichtextType.php
@@ -3,6 +3,8 @@
 namespace App\Form\Type;
 
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,6 +19,22 @@ class RichtextType extends TextareaType
                 'rows' => 10,
             ]
         ]);
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options) : void
+    {
+        $builder->addModelTransformer(new CallbackTransformer(
+            function($db_value) {
+                return $db_value;
+            },
+            function($user_input) {
+                if ($user_input == '<p>&nbsp;</p>') {
+                    return null;
+                } else {
+                    return $user_input;
+                }
+            }
+        ));
     }
 
     public function getBlockPrefix() : string
