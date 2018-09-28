8bbf7240025308729ae905bdddcd4791c2c8b8a2
Samu Juvonen <samu.juvonen@kirjastot.fi>
2018-09-28 12:40:28 +0300

    Update Fontawesome stuff

--- public/js/init.webpack.js
+++ public/js/init.webpack.js
@@ -1,6 +1,4 @@
-import "@fortawesome/fontawesome";
-import "@fortawesome/fontawesome-free-regular";
-import "@fortawesome/fontawesome-free-solid";
+import "@fortawesome/fontawesome-free/js/all";
 
 import "jquery/dist/jquery.min";
 import "moment/min/locales.min";
