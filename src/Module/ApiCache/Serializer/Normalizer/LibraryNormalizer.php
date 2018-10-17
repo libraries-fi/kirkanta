411cfcfde4a487442964dec1b196fdfae877d2bf
Samu Juvonen <samu.juvonen@kirjastot.fi>
2018-10-17 14:49:16 +0300

    Add coverPhoto to library document

---
+++ src/Module/ApiCache/Serializer/Normalizer/LibraryNormalizer.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Module\ApiCache\Serializer\Normalizer;
+
+use App\Entity\LibraryInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class LibraryNormalizer implements NormalizerInterface
+{
+    private $inner;
+
+    public function __construct(NormalizerInterface $inner)
+    {
+        $this->inner = $inner;
+    }
+
+    public function supportsNormalization($data, $format = null)
+    {
+        return $data instanceof LibraryInterface;
+    }
+
+    public function normalize($object, $format = null, array $context = [])
+    {
+        $values = $this->inner->normalize($object, $format, $context);
+        $values['coverPhoto'] = $values['pictures'][0]['files'] ?? null;
+        return $values;
+    }
+}
