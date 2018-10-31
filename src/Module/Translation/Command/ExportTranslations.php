4226f8994b81c7d0b8801b1aa20b4fcc008ea14f
Samu Juvonen <samu.juvonen@kirjastot.fi>
2018-10-31 13:40:42 +0200

    Commands to import/export translations

---
+++ src/Module/Translation/Command/ExportTranslations.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Module\Translation\Command;
+
+use Doctrine\DBAL\Connection;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Yaml\Yaml;
+
+class ExportTranslations extends Command
+{
+    private $db;
+    private $fs;
+
+    public function __construct(Connection $db, Filesystem $fs)
+    {
+        parent::__construct();
+
+        $this->db = $db;
+        $this->fs = $fs;
+    }
+
+    protected function configure() : void
+    {
+        $this
+            ->setName('translation:export')
+            ->setDescription('Export translations to a file')
+            ->addArgument('filename', InputArgument::OPTIONAL, 'Filename', 'translations.dump.yaml')
+            ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) : void
+    {
+        $result = $this->db->query('
+            SELECT domain, locale, id, translation
+            FROM translations
+            ORDER BY domain, id, locale
+        ');
+
+        $data = [];
+
+        foreach ($result as $row) {
+            $data[$row['domain']][$row['id']][$row['locale']] = $row['translation'];
+        }
+
+        $this->fs->dumpFile($input->getArgument('filename'), Yaml::dump($data, 4));
+    }
+}
---
+++ src/Module/Translation/Command/ImportTranslations.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Module\Translation\Command;
+
+use App\Module\Translation\TranslationManager;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Yaml\Yaml;
+
+class ImportTranslations extends Command
+{
+    private $translations;
+
+    public function __construct(TranslationManager $translations)
+    {
+        parent::__construct();
+        $this->translations = $translations;
+    }
+
+    protected function configure() : void
+    {
+        $this
+            ->setName('translation:import')
+            ->setDescription('Import translations from a file to the database')
+            ->addArgument('filename', InputArgument::REQUIRED, 'Filename')
+            ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) : void
+    {
+        $data = Yaml::parseFile($input->getArgument('filename'));
+
+        foreach ($data as $domain => $messages) {
+            foreach ($messages as $id => $translations) {
+                foreach ($translations as $locale => $translation) {
+                    $this->translations->addMessage([
+                        'domain' => $domain,
+                        'locale' => $locale,
+                        'id' => $id,
+                        'translation' => $translation
+                    ]);
+                }
+            }
+        }
+
+        $this->translations->flush();
+    }
+}
