b634755ab708de3d13767b6a416beda8ed9d5967
Samu Juvonen <samu.juvonen@kirjastot.fi>
2018-11-13 13:58:43 +0200

    Add informative comment

--- public/js/widget.select.js
+++ public/js/widget.select.js
@@ -1,3 +1,10 @@
+/**
+ * Replace native <select> boxes with React widgets.
+ *
+ * Main issue is that on Chrome opening a select box will force browser to scroll to top,
+ * if the input is contained inside a sticky bar.
+ */
+
 import React from "react";
 import ReactDOM from "react-dom";
 import { DropdownList } from "react-widgets";
@@ -21,5 +28,4 @@ document.querySelectorAll("select").forEach((select) => {
   select.parentElement.insertBefore(container, select);
   select.style.display = "none";
   ReactDOM.render(widget, container);
-
 });
