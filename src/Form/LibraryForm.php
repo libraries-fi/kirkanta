27b998bfbbc2b22de01a6ebc3f62d264df66a2d4
Samu Juvonen <samu.juvonen@kirjastot.fi>
2018-11-20 17:36:42 +0200

    Constrain React widget width like other form controls too

--- src/Form/LibraryForm.php
+++ src/Form/LibraryForm.php
@@ -30,14 +30,14 @@ class LibraryForm extends FormType
             ->add('main_library', CheckboxType::class, [
                 'required' => false,
             ])
-            ->add('organisation', EntityType::class, [
-                'property_path' => 'organisation',
-                'label' => 'Parent organisation',
-                'required' => false,
-                'class' => Organisation::class,
-                'placeholder' => '-- Select --',
-                'choice_label' => 'name',
-            ])
+            // ->add('organisation', EntityType::class, [
+            //     'property_path' => 'organisation',
+            //     'label' => 'Parent organisation',
+            //     'required' => false,
+            //     'class' => Organisation::class,
+            //     'placeholder' => '-- Select --',
+            //     'choice_label' => 'name',
+            // ])
             ->add('isil', null, [
                 'required' => false,
                 'label' => 'ISIL',
@@ -104,6 +104,7 @@ class LibraryForm extends FormType
                 $event->getForm()->add('organisation', EntityType::class, [
                     'class' => Organisation::class,
                     'required' => false,
+                    'label' => 'Parent organisation',
                     'placeholder' => '-- Select --',
                     'query_builder' => function($repo) use($groups) {
                         return $repo->createQueryBuilder('e')
