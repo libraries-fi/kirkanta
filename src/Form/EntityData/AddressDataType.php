6e609c6c6737bf9e9a6e989e71a6b24cd218814b
Samu Juvonen <samu.juvonen@kirjastot.fi>
2018-11-28 23:06:10 +0200

    Add info text

--- src/Form/EntityData/AddressDataType.php
+++ src/Form/EntityData/AddressDataType.php
@@ -24,6 +24,7 @@ class AddressDataType extends EntityDataType
             ])
             ->add('info', null, [
                 'label' => 'Directions',
+                'help' => 'Short description for locating the building or entrance.',
                 'required' => false,
             ])
             ;
