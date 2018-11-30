7115508cd2454f5bfdbaa55eb30dd4e0a0d316c0
Samu Juvonen <samu.juvonen@kirjastot.fi>
2018-11-30 20:11:20 +0200

    Have to sync addresses before libraries

--- src/Module/LegacyApiCompatibility/Command/SyncLegacyDatabase.php
+++ src/Module/LegacyApiCompatibility/Command/SyncLegacyDatabase.php
@@ -152,6 +152,18 @@ class SyncLegacyDatabase extends Command
             $row['region_id'] = 1003;
         });
 
+        $this->synchronize('addresses', 'addresses', ['id', 'city_id', 'zipcode', 'box_number', 'ST_AsText(coordinates) AS coordinates'], function(&$row) {
+
+            if ($row['coordinates']) {
+                list($lon, $lat) = explode(' ', substr($row['coordinates'], 6, -1));
+                $row['coordinates'] = "{$lat}, {$lon}";
+            }
+
+            // In legacy DB, coordinates exist in organisations table.
+            $this->cache->coords[$row['id']] = $row['coordinates'];
+            unset($row['coordinates']);
+        });
+
         $this->synchronize('organisations', 'organisations', [
             'role',
             // 'type',
@@ -203,18 +215,6 @@ class SyncLegacyDatabase extends Command
                 unset($data['phone_id']);
                 unset($data['slug']);
             }
-        });
-
-        $this->synchronize('addresses', 'addresses', ['id', 'city_id', 'zipcode', 'box_number', 'ST_AsText(coordinates) AS coordinates'], function(&$row) {
-
-            if ($row['coordinates']) {
-                list($lon, $lat) = explode(' ', substr($row['coordinates'], 6, -1));
-                $row['coordinates'] = "{$lat}, {$lon}";
-            }
-
-            // In legacy DB, coordinates exist in organisations table.
-            $this->cache->coords[$row['id']] = $row['coordinates'];
-            unset($row['coordinates']);
         });
 
         $this->synchronize('pictures', 'pictures', ['id', 'filename', 'created', 'parent_id', 'cover'], function(&$row) {
