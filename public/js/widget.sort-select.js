8658ef6a09978aa69f533787afb3b2c6619b9148
Samu Juvonen <samu.juvonen@kirjastot.fi>
2018-12-19 14:37:04 +0200

    Copy fixed sort-select.js from branch default_langcode

--- public/js/widget.sort-select.js
+++ public/js/widget.sort-select.js
@@ -7,11 +7,21 @@
  */
 
 document.querySelectorAll("select").forEach((select) => {
-  const options = Array.from(select.children).sort((a, b) => {
+  const options = Array.from(select.children);
+  const separator = select.querySelector('[disabled]');
+  const preferredChoicesUntil = options.indexOf(separator);
+  const sortableChildren = options.slice(preferredChoicesUntil + 1);
+
+  sortableChildren.sort((a, b) => {
+    if (a.tagName == 'OPTGROUP') {
+      return -1
+    } else if (b.tagName == 'OPTGROUP') {
+      return 1;
+    }
     return a.text.localeCompare(b.text, 'fi');
   });
 
-  options.forEach((option, i) => {
+  sortableChildren.forEach((option, i) => {
     // Keep placeholder options at the top.
     if (option.value) {
       select.appendChild(option);
