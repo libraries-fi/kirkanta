3e995078642d4ae195e7411ac1c0376ab34e5ba2
Samu Juvonen <samu.juvonen@kirjastot.fi>
2018-12-19 15:57:33 +0200

    Hide ownership select box from Library resources

--- src/Form/EntityFormType.php
+++ src/Form/EntityFormType.php
@@ -28,44 +28,47 @@ abstract class EntityFormType extends FormType
         parent::configureOptions($options);
         $options->setDefaults([
             'context_entity' => null,
+            'disable_ownership' => false,
         ]);
     }
 
     public function form(FormBuilderInterface $builder, array $options) : void
     {
-        $builder->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event) {
-            $entity = $event->getData();
-            $form = $event->getForm();
+        if (!$options['disable_ownership']) {
+            $builder->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event) {
+                $entity = $event->getData();
+                $form = $event->getForm();
 
-            if (!$form->getParent() && $entity instanceof GroupOwnership) {
-                $help = 'Changing this value will change user permissions for this record.';
+                if (!$form->getParent() && $entity instanceof GroupOwnership) {
+                    $help = 'Changing this value will change user permissions for this record.';
 
-                if ($this->auth->isGranted('foobar')) {
-                    $preferred_groups = $entity->getOwner()->getTree();
-                    $form->add('owner', EntityType::class, [
-                        'help' => $help,
-                        'class' => UserGroup::class,
-                        'query_builder' => function($repo) {
-                            return $repo->createQueryBuilder('e');
-                        },
-                        'preferred_choices' => $preferred_groups
-                    ]);
-                } else {
-                    $group = $this->auth->getUser()->getGroup();
-                    do {
-                        $choices[] = $group;
-                    } while ($group = $group->getParent());
+                    if ($this->auth->isGranted('foobar')) {
+                        $preferred_groups = $entity->getOwner()->getTree();
+                        $form->add('owner', EntityType::class, [
+                            'help' => $help,
+                            'class' => UserGroup::class,
+                            'query_builder' => function($repo) {
+                                return $repo->createQueryBuilder('e');
+                            },
+                            'preferred_choices' => $preferred_groups
+                        ]);
+                    } else {
+                        $group = $this->auth->getUser()->getGroup();
+                        do {
+                            $choices[] = $group;
+                        } while ($group = $group->getParent());
 
-                    $form->add('owner', EntityType::class, [
-                        'help' => $help,
-                        'class' => UserGroup::class,
-                        'choices' => $choices,
-                        'attr' => [
-                            'data-no-sort' => true
-                        ]
-                    ]);
+                        $form->add('owner', EntityType::class, [
+                            'help' => $help,
+                            'class' => UserGroup::class,
+                            'choices' => $choices,
+                            'attr' => [
+                                'data-no-sort' => true
+                            ]
+                        ]);
+                    }
                 }
-            }
-        });
+            });
+        }
     }
 }
