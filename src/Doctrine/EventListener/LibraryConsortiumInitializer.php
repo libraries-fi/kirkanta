27b4abd5bde70ee5912c0681797674575b855890
Samu Juvonen <samu.juvonen@kirjastot.fi>
2019-01-17 18:54:03 +0200

    Expand initializer to be used for re-assigning the consortium on libraries after it changes for a city

--- src/Doctrine/EventListener/LibraryConsortiumInitializer.php
+++ src/Doctrine/EventListener/LibraryConsortiumInitializer.php
@@ -7,6 +7,9 @@ use Doctrine\Common\EventSubscriber;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Events;
 
+/**
+ * Initializes municipal libraries with the consortium entity from their city.
+ */
 class LibraryConsortiumInitializer
 {
     public function prePersist(Library $library, LifecycleEventArgs $args) : void
@@ -24,9 +27,11 @@ class LibraryConsortiumInitializer
         $allowed_types = ['municipal', 'main_library', 'music', 'regional', 'mobile'];
 
         if (in_array($library->getType(), $allowed_types)) {
-            if (!$library->getConsortium()) {
-                $library->setConsortium($library->getCity()->getConsortium());
-            }
+            /**
+             * Always set consortium. This way we can use this initializer to update
+             * the consortium binding whenever the consortium for a city is changed.
+             */
+            $library->setConsortium($library->getCity()->getConsortium());
         } else {
             // Maybe the library's type was changed and it doesn't qualify for a consortium anymore.
             $library->setConsortium(null);
